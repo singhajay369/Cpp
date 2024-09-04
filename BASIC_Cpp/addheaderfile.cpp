0c02401aa52f0af9ab04665ad4dc7514c892f7c6
singhajay <singhajay2542004@gmail.com>
2024-09-04 16:41:01 +0530

    add

---
+++ BASIC_Cpp/addheaderfile.cpp
@@ -0,0 +1,13 @@
+// THERE ARE 2 TYPES OF HEADER FILE 
+// 1 system header file 
+// 2 userdefine header file
+// to add a file in header file it must be present in the current directory 
+// syntax : #include"a"
+#include<iostream>
+#include"A"
+using namespace std;
+int main (){
+    cout << "Hello duniya";
+
+    return 0;
+}
